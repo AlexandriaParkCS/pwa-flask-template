a7935eb8579b00e0f5334d80a42dfd2146579fdc
Mr. Andonov <emil.andonov1@education.nsw.gov.au>
2026-01-21 13:39:49 +1100

    Adding SQL only example.

---
+++ src/sqldb.sql
@@ -0,0 +1,22 @@
+-- database: ../runtime/db/app.db
+
+-- NOTE:
+-- Usually each SQL statement here will be placed in its own SQL file. 
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL);
+
+-- Add user
+INSERT INTO users (username, email) VALUES ('luke', 'may4bwithu', 'luke@starwars.com');
+
+-- Get user by user name
+SELECT id, username, email FROM users WHERE username = 'luke';
+
+-- Update user
+UPDATE users SET email = 'skywalker@starwars.com' WHERE username = 'luke';
+
+-- Delete user
+DELETE FROM users WHERE username = 'luke';
